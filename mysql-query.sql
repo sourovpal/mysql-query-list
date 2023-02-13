412a57232dea40e53d2aea1cb6cbc9344b714c2e
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2023-02-13 14:31:37 +0600

    Update mysql-query.sql

--- mysql-query.sql
+++ mysql-query.sql
@@ -1,4 +1,8 @@
+SHOW INDEX FROM table_name;
+
 ALTER TABLE `table_name` ADD FULLTEXT KEY `name_email` (`name`,`email`);
+
+SELECT * FROM table_name WHERE MATCH(name_email) AGAINST('search text' IN BOOLEAN MODE);
 
 
 
