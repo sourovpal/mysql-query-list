00e14b45126871ded988d00b6407059316b03289
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2023-02-22 17:43:35 +0600

    Update mysql-query.sql

--- mysql-query.sql
+++ mysql-query.sql
@@ -1,3 +1,23 @@
+
+SELECT DISTINCT name FROM `users`;
+
+SELECT COUNT(DISTINCT name) as user_name FROM `users`;
+
+SELECT * FROM posts WHERE user_id IN (SELECT DISTINCT id FROM users);
+SELECT * FROM posts WHERE user_id IN (SELECT DISTINCT id FROM users WHERE id >2 );
+
+SELECT * FROM users WHERE name IS NULL;
+SELECT * FROM users WHERE name IS NOT NULL;
+
+--                          if true 0 false 1;
+UPDATE posts SET title = IF(id!='1', '0','1');
+
+--                          if true 100 false 0;
+UPDATE posts SET title = IF(id ='1', '100','0');
+
+SELECT * FROM posts WHERE EXISTS (SELECT id FROM users WHERE  id = 100);
+
+
 SHOW INDEX FROM table_name;
 
 ALTER TABLE `table_name` ADD FULLTEXT KEY `name_email` (`name`,`email`);
